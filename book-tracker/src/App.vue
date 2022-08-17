2b8924d0704b14ce2722b3d21cb1e40be56995af
Manuletti Desktop at SirBlvrd <actimel@yandex.ru>
2022-08-18 01:46:23 +0300

    quasar testing

--- book-tracker/src/App.vue
+++ book-tracker/src/App.vue
@@ -1,3 +1,39 @@
 <template>
-  <h1>Hello world</h1>
+  <q-circular-progress
+    :value="value"
+    size="300px"
+    color="orange"
+    track-color="blue"
+    class="q-ma-md"
+    reverse
+    angle="90"
+    show-value
+    :min="min"
+    :max="max"
+    thickness="0.5"
+    animation-speed="500"
+  />
+  <q-slider
+    name="pages"
+    label-always
+    :min="min"
+    :max="max"
+    v-model="value"
+    color="blue"
+    track-color="orange"
+  />
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+let value = ref(456);
+const min = ref(0)
+const max = ref(756)
+
+
+</script>
+
+<style lang="sass">
+$primarycolor: red
+</style>
